db12d4abbdaf39b2aad8db12c6bee82dfc9d0576
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-14 15:25:02 +0900

    feat(exception): add message

---
+++ src/main/java/attendance/view/Exception.java
@@ -0,0 +1,8 @@
+package attendance.view;
+
+public class Exception {
+    public static final String INVALID_INPUT = "[ERROR] 잘못된 형식을 입력하였습니다.";
+    public static final String INVALID_NAME = "[ERROR] 등록되지 않은 닉네임입니다.";
+
+    public static final String INVALID_FILE = "[ERROR] 존재하지 않는 파일입니다.";
+}
