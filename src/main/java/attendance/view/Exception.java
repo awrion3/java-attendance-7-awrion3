7951a1c47b6882edc6c8a83f4ade2cd3aab9d201
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-14 15:46:36 +0900

    feat(exception): add format exception

--- src/main/java/attendance/view/Exception.java
+++ src/main/java/attendance/view/Exception.java
@@ -1,8 +1,17 @@
 package attendance.view;
 
+import attendance.util.Calendar;
+
 public class Exception {
     public static final String INVALID_INPUT = "[ERROR] 잘못된 형식을 입력하였습니다.";
     public static final String INVALID_NAME = "[ERROR] 등록되지 않은 닉네임입니다.";
 
     public static final String INVALID_FILE = "[ERROR] 존재하지 않는 파일입니다.";
+
+    public static String formatMessage() {
+        int month = Calendar.checkTodayMonth();
+        int day = Calendar.checkTodayDayOfMonth();
+        String dayOfWeek = Calendar.checkTodayDayOfWeek();
+        return String.format("[ERROR] %d월 %d일 %s요일은 등교일이 아닙니다.", month, day, dayOfWeek);
+    }
 }
