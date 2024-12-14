7456a26fe71110265db6498a2989d4b14af578ee
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-14 17:39:41 +0900

    feat(input): add ask for second option

--- src/main/java/attendance/view/Input.java
+++ src/main/java/attendance/view/Input.java
@@ -17,4 +17,18 @@ public class Input {
         return Console.readLine();
     }
 
+    public static String askChangeCrewName() {
+        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static String askChangeCrewDay() {
+        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static String askChangeCrewTime() {
+        System.out.println("언제로 변경하겠습니까?");
+        return Console.readLine();
+    }
 }
