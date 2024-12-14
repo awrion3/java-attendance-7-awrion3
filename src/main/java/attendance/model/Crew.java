1218667fd64d6b8bd141a44ad026f01f022981c9
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-14 16:07:22 +0900

    feat(crew): add attend attribute

--- src/main/java/attendance/model/Crew.java
+++ src/main/java/attendance/model/Crew.java
@@ -9,7 +9,7 @@ public class Crew {
     private String name;
     private LocalDate date;
     private LocalTime time;
-    private String Attend;
+    private String attend = "(결석)";
 
     //LocalDate date = LocalDate.of(year, month, dayOfMonth);
     //        LocalTime time = LocalTime.of(hour, minute);
@@ -33,9 +33,39 @@ public class Crew {
         crew.time = localTime;
     }
 
-//    private void checkAttend(){
-//        if ()
-//    }
+    public static void checkMonday(Crew crew) {
+        if (crew.time.isBefore(LocalTime.parse("13:00"))) {
+            crew.attend = "(출석)";
+            return;
+        }
+        if (crew.time.equals(LocalTime.parse("13:00"))) {
+            crew.attend = "(출석)";
+            return;
+        }
+        if (crew.time.isAfter(LocalTime.parse("13:05"))) {
+            crew.attend = "(지각)";
+        }
+        if (crew.time.isAfter(LocalTime.parse("13:30"))) {
+            crew.attend = "(결석)";
+        }
+    }
+
+    public static void checkOtherDay(Crew crew) {
+        if (crew.time.isBefore(LocalTime.parse("10:00"))) {
+            crew.attend = "(출석)";
+            return;
+        }
+        if (crew.time.equals(LocalTime.parse("10:00"))) {
+            crew.attend = "(출석)";
+            return;
+        }
+        if (crew.time.isAfter(LocalTime.parse("10:05"))) {
+            crew.attend = "(지각)";
+        }
+        if (crew.time.isAfter(LocalTime.parse("10:30"))) {
+            crew.attend = "(결석)";
+        }
+    }
 
     public String getName() {
         return name;
@@ -47,5 +77,9 @@ public class Crew {
 
     public LocalTime getTime() {
         return time;
+    }
+
+    public String getAttend() {
+        return attend;
     }
 }
