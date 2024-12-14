045c48deb0278e826127ba7411ae4568ea8ce702
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-14 17:27:05 +0900

    feat(controller): add repeat

--- src/main/java/attendance/controller/Controller.java
+++ src/main/java/attendance/controller/Controller.java
@@ -1,5 +1,6 @@
 package attendance.controller;
 
+import attendance.model.Crew;
 import attendance.model.Crews;
 import attendance.model.Option;
 import attendance.util.Validator;
@@ -8,34 +9,46 @@ import attendance.view.Output;
 
 public class Controller {
     public void start() {
-
-        // TODO 데이터 초기화
         Crews crews = new Crews();
-//        while (true){
-//            String reply = null;
-//            if (reply.equals("Q"))
-//                break;
-//        }
 
-        Output.printGreetings();
-        Option option = new Option(Input.askOption());
+        while (true) {
+            Output.printGreetings();
+            Option option = new Option(Input.askOption());
 
 //        for (Crew crew : crews.getCrews()) {
 //            System.out.println(crew.getName());
 //            System.out.println(crew.getTime());
 //        }
 
-        // TODO 1
-        if (option.getOption().equals("1")) {
-            Validator.validateClass();
-            String name = Input.askAttendCheckName();
-            Validator.validateCrewName(crews, name);
-            String clockTime = Input.askAttendCheckTime();
-            Validator.validateTimeFormat(clockTime);
-            crews.updateCrewTime(name, clockTime);
-            crews.checkAttend(name);
-            Output.printCrewStatus(name, crews);
-        }
+            if (option.getOption().equals("1")) {
+                Validator.validateClass();
+                String name = Input.askAttendCheckName();
+                Validator.validateCrewName(crews, name);
+                String clockTime = Input.askAttendCheckTime();
+                Validator.validateTimeFormat(clockTime);
+                crews.updateCrewTime(name, clockTime);
+                crews.checkAttend(name);
+                Output.printCrewStatus(name, crews);
+            }
 
+            if (option.getOption().equals("2")) {
+
+            }
+
+            if (option.getOption().equals("3")) {
+                String name = Input.askAttendCheckName();
+                Validator.validateCrewName(crews, name);
+                Crew id = crews.findCrew(name);
+                Output.printCrewStatus(name, crews);
+            }
+
+            if (option.getOption().equals("4")) {
+
+            }
+
+            if (option.equals("Q")) {
+                break;
+            }
+        }
     }
 }
