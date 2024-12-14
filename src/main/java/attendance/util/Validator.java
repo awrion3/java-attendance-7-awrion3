31e1ad6780cff2f96a5361163801d93df3033d5c
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-14 15:24:40 +0900

    feat(validator): add validate

---
+++ src/main/java/attendance/util/Validator.java
@@ -0,0 +1,33 @@
+package attendance.util;
+
+import static attendance.view.Exception.INVALID_INPUT;
+import static attendance.view.Exception.INVALID_NAME;
+
+import attendance.model.Crews;
+
+public class Validator {
+    public static void validatePattern(String input) {
+        if (!input.matches("^[1-4|Q]$")) {
+            throw new IllegalArgumentException(INVALID_INPUT);
+        }
+    }
+
+    public static void validateCrewName(Crews crews, String name) {
+        boolean isMatch = crews.getCrews()
+                .stream()
+                .anyMatch(crew -> crew.getName().equals(name));
+        if (!isMatch) {
+            throw new IllegalArgumentException(INVALID_NAME);
+        }
+    }
+
+    public static void validateTimeFormat(String time) {
+        if (!time.matches("^[0-9]{2}:[0-9]{2}$")) {
+            throw new IllegalArgumentException(INVALID_INPUT);
+        }
+    }
+    //try {
+    //            return LocalDate.parse(time);
+    //        } catch (DateTimeParseException e) {
+    //           }
+}
